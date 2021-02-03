c11843cbb4c3573330e10c66252ec4cd29637a0f
Radish <53844660+radradix@users.noreply.github.com>
2021-02-03 13:12:33 -0800

    Update theme.js
    I gave up on trying to deal with localstorage. I don't think it's really possible unless I buy my own domain, since it only worked on localhost. This option works, although the dark theme state doesn't persist across page loads. :(

--- theme.js
+++ theme.js
@@ -1,76 +1,12 @@
-//const theme = document.querySelector("#theme-link");
-//document.cookie = "light=false";
-console.log(localStorage);
+var counter = 1;
 
 function flip() {
-/*    if(document.cookie.includes("light=true")){
-        document.cookie = "light=false";
-        document.getElementById("theme-link").href="dark-theme.css";
-    } else if(document.cookie.includes("light=false")){
-        document.cookie = "light=true";
+    counter++;
+    if(counter%2===0){ // if it's odd, meaning it's currently light theme
+        document.getElementById("theme-link").href="dark-theme.css"
+        return console.log("Successful change to dark-theme.css")
+    } else if(counter%2===1){
         document.getElementById("theme-link").href="light-theme.css";
+        return console.log("Successful change to light-theme.css")
     }
-    console.log("cookies after flip: " + document.cookie);*/
-
-    const currentTheme = localStorage.getItem("theme");
-    if (currentTheme == "light") {
-      document.getElementById("theme-link").href="dark-theme.css";
-      localStorage.setItem("theme","dark");
-    } else if (currentTheme == "dark") {
-      document.getElementById("theme-link").href="light-theme.css";
-      localStorage.setItem("theme","light");
-    }
-    console.log(localStorage);
 }
-
-
-/*
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// Select the button
-const btn = document.querySelector(".btn-toggle");
-// Check for dark mode preference at the OS level
-//const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-// Get the user's theme preference from local storage, if it's available
-const currentTheme = localStorage.getItem("theme");
-// If the user's preference in localStorage is dark...
-if (currentTheme == "dark") {
-  // ...let's toggle the .dark-theme class on the body
-  document.body.classList.toggle("dark-mode");
-// Otherwise, if the user's preference in localStorage is light...
-} else if (currentTheme == "light") {
-  // ...let's toggle the .light-theme class on the body
-  document.body.classList.toggle("light-mode");
-}
-
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // If the user's OS setting is dark and matches our .dark-mode class...
-  if (prefersDarkScheme.matches) {
-    // ...then toggle the light mode class
-    document.body.classList.toggle("light-mode");
-    // ...but use .dark-mode if the .light-mode class is already on the body,
-    var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
-  } else {
-    // Otherwise, let's do the same thing, but for .dark-mode
-    document.body.classList.toggle("dark-mode");
-    var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
-  }
-  // Finally, let's save the current preference to localStorage to keep using it
-  localStorage.setItem("theme", theme);
-});
-*/
-
